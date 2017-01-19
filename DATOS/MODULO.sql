10de40ee8707e5d2478e0694ada93a93cec04573
joseliza <joseliza@debian>
2017-01-19 18:08:16 +0100

    Cambios tras repasar modelo ER y comenzar a extraer algunos datos del primer modelo de calificcaiones

---
+++ DATOS/MODULO.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+-- Archivo creado  - jueves-enero-19-2017   
+--------------------------------------------------------
+REM INSERTING into CALIFICACIONES1.MODULO
+SET DEFINE OFF;
+Insert into CALIFICACIONES1.MODULO (CODIGO,NOMBRE,HORAS,NIVEL) values ('0377','ADMINISTRACIÓN DE SISTEMAS GESTORES DE BASES DE
+ DATOS','63','2GS');
+Insert into CALIFICACIONES1.MODULO (CODIGO,NOMBRE,HORAS,NIVEL) values ('0372','GESTIÓN DE BASES DE
+ DATOS','192','1GS');
+Insert into CALIFICACIONES1.MODULO (CODIGO,NOMBRE,HORAS,NIVEL) values ('0227','SERVICIOS EN RED','147','2GM');
---
+++ DATOS/PROFESOR.sql
@@ -0,0 +1,6 @@
+--------------------------------------------------------
+-- Archivo creado  - jueves-enero-19-2017   
+--------------------------------------------------------
+REM INSERTING into CALIFICACIONES1.PROFESOR
+SET DEFINE OFF;
+Insert into CALIFICACIONES1.PROFESOR (DNI,NOMBRE,APELLIDOS) values ('44288744J','José Manuel','Lizana Jiménez');
